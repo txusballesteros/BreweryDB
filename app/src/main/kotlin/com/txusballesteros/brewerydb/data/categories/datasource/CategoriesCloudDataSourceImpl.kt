f00d6bfce1f64a70a21b135d7e0586ef7cca7422
Txus Ballesteros <txus.ballesteros@gmail.com>
2017-02-12 17:42:04 +0100

    Builded Categories Data Sources layer

---
+++ app/src/main/kotlin/com/txusballesteros/brewerydb/data/categories/datasource/CategoriesCloudDataSourceImpl.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright Txus Ballesteros 2017 (@txusballesteros)
+ *
+ * This file is part of Foobar.
+ *
+ * Foobar is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Foobar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact: Txus Ballesteros <txus.ballesteros@gmail.com>
+ */
+package com.txusballesteros.brewerydb.data.categories.datasource
+
+import com.txusballesteros.brewerydb.api.categories.CategoriesApi
+import com.txusballesteros.brewerydb.api.model.CategoryApiModelMapper
+import com.txusballesteros.brewerydb.data.model.CategoryDataModel
+import com.txusballesteros.brewerydb.data.styles.datasource.CategoriesCloudDataSource
+import javax.inject.Inject
+
+class CategoriesCloudDataSourceImpl @Inject constructor(private val api: CategoriesApi,
+                                                        private val categoriesApiModelMapper: CategoryApiModelMapper):
+                                    CategoriesCloudDataSource {
+  override fun getCategories(): List<CategoryDataModel> {
+    val response = api.getCategories()
+    val result = categoriesApiModelMapper.map(response.categories)
+    return result.sortedBy(CategoryDataModel::name)
+  }
+}
---
+++ app/src/main/kotlin/com/txusballesteros/brewerydb/data/categories/datasource/CategoriesInMemoryLocalDataSource.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright Txus Ballesteros 2017 (@txusballesteros)
+ *
+ * This file is part of Foobar.
+ *
+ * Foobar is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Foobar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact: Txus Ballesteros <txus.ballesteros@gmail.com>
+ */
+package com.txusballesteros.brewerydb.data.categories.datasource
+
+import com.txusballesteros.brewerydb.data.AbsInMemoryDataSource
+import com.txusballesteros.brewerydb.data.model.CategoryDataModel
+import javax.inject.Inject
+
+class CategoriesInMemoryLocalDataSource @Inject constructor(): AbsInMemoryDataSource<CategoryDataModel>(),
+                                                               CategoriesLocalDataSource {
+  override fun store(categories: List<CategoryDataModel>) {
+    addAll(categories)
+  }
+
+  override fun getCategories(): List<CategoryDataModel> {
+    return getAll()
+  }
+}
