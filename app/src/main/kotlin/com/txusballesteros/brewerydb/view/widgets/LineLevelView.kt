9e48e6c6b19b87f49db61b7b1f01f6e821de9251
Txus Ballesteros <txus.ballesteros@gmail.com>
2017-02-22 10:03:30 +0100

    Refactoring Line Chart View

--- app/src/main/kotlin/com/txusballesteros/brewerydb/view/widgets/LineLevelView.kt
+++ app/src/main/kotlin/com/txusballesteros/brewerydb/view/widgets/LineLevelView.kt
@@ -28,20 +28,26 @@ import android.view.View
 
 class LineLevelView: View {
   companion object {
+    val EMPTY_VALUE: Float = 0f
     val DASH_SIZE_IN_DP: Float = 5f
     val MAIN_LINE_WEIGHT_IN_DP: Float = 2f
     val REFERENCE_LINE_WEIGHT_IN_DP: Float = 0.5f
     val MAIN_LINE_COLOR: Int = Color.parseColor("#fff57f17")
     val REFERENCE_LINE_COLOR: Int = Color.parseColor("#ffe5e5e5")
+    val VALUES_WINDOW_FILL_COLOR: Int = Color.parseColor("#ffffecce")
   }
-  var minValue: Float = 0f
-  var maxValue: Float = 0f
-  var value: Float = 0f
+  var minValue: Float = EMPTY_VALUE
+  var maxValue: Float = EMPTY_VALUE
+  var value: Float = EMPTY_VALUE
+  private var maximumReferenceValue: Float = 12f
+  private var minimumReferenceValue: Float = 0f
   private var drawingArea: RectF = RectF()
   private var valueColor: Int = MAIN_LINE_COLOR
   private var referenceColor: Int = REFERENCE_LINE_COLOR
+  private var valuesWindowColor: Int = VALUES_WINDOW_FILL_COLOR
   private val mainLinePaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
   private val referenceLinePaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+  private val valuesWindowPaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
   private val dashSize = dp2px(DASH_SIZE_IN_DP)
 
   constructor(context: Context?) : super(context)
@@ -59,6 +65,8 @@ class LineLevelView: View {
     referenceLinePaint.strokeCap = Paint.Cap.ROUND
     referenceLinePaint.color = referenceColor
     referenceLinePaint.pathEffect = DashPathEffect(floatArrayOf(dashSize, dashSize), 0f)
+    valuesWindowPaint.style = Paint.Style.FILL
+    valuesWindowPaint.color = valuesWindowColor
     if (isInEditMode) {
       minValue = 4.5f
       maxValue = 5.5f
@@ -79,30 +87,39 @@ class LineLevelView: View {
   }
 
   override fun onDraw(canvas: Canvas?) {
+    drawValuesWindow(canvas)
     drawReferenceLine(canvas, minValue)
     drawReferenceLine(canvas, maxValue)
-    if (value > minValue) {
+    if (value > minimumReferenceValue) {
       drawMainLine(canvas)
     }
   }
 
+  private fun drawValuesWindow(canvas: Canvas?) {
+    if (maxValue > minValue) {
+      val maximumValueY = getLevel(maxValue)
+      val minimumValueY = getLevel(minValue)
+      canvas?.drawRect(RectF(drawingArea.left, maximumValueY, drawingArea.right, minimumValueY), valuesWindowPaint)
+    }
+  }
+
   private fun drawReferenceLine(canvas: Canvas?, value: Float) {
-    val levelX = drawingArea.bottom - getLevel(value)
-    canvas?.drawLine(drawingArea.left, levelX, drawingArea.right, levelX, referenceLinePaint)
+    val levelY = getLevel(value)
+    canvas?.drawLine(drawingArea.left, levelY, drawingArea.right, levelY, referenceLinePaint)
   }
 
   private fun drawMainLine(canvas: Canvas?) {
     val offsetInX = 0.15f
     val offsetInY = 0.05f
-    val levelX = getLevel(this.value)
+    val levelY = getLevel(this.value)
     val x1_A = drawingArea.centerX() - (drawingArea.centerX() * offsetInX)
     val y1_A = drawingArea.bottom - (drawingArea.height() * offsetInY)
     val x2_A = drawingArea.centerX() - (drawingArea.centerX() * offsetInX)
-    val y2_A = drawingArea.bottom - levelX
+    val y2_A = levelY
     val x3_A = drawingArea.centerX()
-    val y3_A = drawingArea.bottom - levelX
+    val y3_A = levelY
     val x1_B = drawingArea.centerX() + (drawingArea.centerX() * offsetInX)
-    val y1_B = drawingArea.bottom - levelX
+    val y1_B = levelY
     val x2_B = drawingArea.centerX() + (drawingArea.centerX() * offsetInX)
     val y2_B = drawingArea.bottom - (drawingArea.height() * offsetInY)
     val x3_B = drawingArea.right
@@ -115,12 +132,12 @@ class LineLevelView: View {
   }
 
   private fun getLevel(value: Float): Float {
-    val minimumValue = Math.min(minValue, this.value)
-    val maximumValue = Math.max(maxValue, this.value)
+    val minimumValue = Math.min(minimumReferenceValue, this.value)
+    val maximumValue = Math.max(maximumReferenceValue, this.value)
     val difference = maximumValue - minimumValue
     val referenceValue = value - minimumValue
     val scale = drawingArea.height() / difference
-    return referenceValue * scale
+    return drawingArea.bottom - (referenceValue * scale)
   }
 
   private fun dp2px(value: Float)
