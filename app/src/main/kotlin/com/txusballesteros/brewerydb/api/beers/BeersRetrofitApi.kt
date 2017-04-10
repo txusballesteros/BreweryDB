57a859a64e18f35e95027f0dd3048783fdf1ab19
Txus Ballesteros <txus.ballesteros@gmail.com>
2017-04-10 09:45:45 +0200

    Adding Beer Labels Place holders

--- app/src/main/kotlin/com/txusballesteros/brewerydb/api/beers/BeersRetrofitApi.kt
+++ app/src/main/kotlin/com/txusballesteros/brewerydb/api/beers/BeersRetrofitApi.kt
@@ -45,6 +45,7 @@ class BeersRetrofitApi @Inject constructor(private val service: BeersRetrofitSer
           query.isOrganic,
           query.breweryId,
           query.styleId,
+          query.withLabels,
           query.status,
           page)
       val response = call.execute()
