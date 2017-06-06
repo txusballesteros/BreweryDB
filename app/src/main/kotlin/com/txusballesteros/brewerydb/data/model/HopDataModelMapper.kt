1db98c47f5bb29b82c89f31973f45445832377aa
Txus Ballesteros <txus.ballesteros@gmail.com>
2017-06-06 22:04:20 +0200

    Refactoring Ingredients Data Mappers

---
+++ app/src/main/kotlin/com/txusballesteros/brewerydb/data/model/HopDataModelMapper.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright Txus Ballesteros 2017 (@txusballesteros)
+ *
+ * This file is part of Foobar.
+ *
+ * Foobar is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Foobar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact: Txus Ballesteros <txus.ballesteros@gmail.com>
+ */
+package com.txusballesteros.brewerydb.data.model
+
+import com.txusballesteros.brewerydb.domain.model.Hop
+
+fun mapToDomain(source: HopDataModel) = Hop(source.id,
+                                            source.name,
+                                            source.description,
+                                            source.alphaAcidMin,
+                                            source.alphaAcidMax,
+                                            source.betaAcidMin,
+                                            source.betaAcidMax,
+                                            source.humuleneMin,
+                                            source.humuleneMax,
+                                            source.caryophylleneMin,
+                                            source.caryophylleneMax,
+                                            source.cohumuloneMin,
+                                            source.cohumuloneMax,
+                                            source.myrceneMin,
+                                            source.myrceneMax,
+                                            source.farneseneMin,
+                                            source.farneseneMax,
+                                            source.countryOfOrigin,
+                                            mapToDomain(source.country))
